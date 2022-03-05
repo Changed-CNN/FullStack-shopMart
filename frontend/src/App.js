004a785ec7f631fbd7539dd3305c556c49ef7909
Melo Feng <melo_feng@Melo-MacBook-Air.local>
2022-03-04 19:19:41 -0500

    build user update screen and functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
-
+import UserEditScreen from './screens/UserEditScreen';
 
 
 
@@ -38,6 +38,7 @@ function App() {
             <Route path='/placeorder' element={<PlaceOrderScreen/>}></Route>
             <Route path='/order/:id' element={<OrderScreen/>}></Route>
             <Route path='/admin/userlist' element={<UserListScreen/>}></Route>
+            <Route path='/admin/user/:id/edit' element={<UserEditScreen/>}></Route>
           </Routes>
         </Container>
       </main>

e768ae85f77d94c12ee7f9f8983f86c1fce37522
Melo Feng <melo_feng@Melo-MacBook-Air.local>
2022-03-05 20:08:54 -0500

    build up productlist productcreate productupdate screen and functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,8 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
-
+import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 
 function App() {
@@ -39,6 +40,8 @@ function App() {
             <Route path='/order/:id' element={<OrderScreen/>}></Route>
             <Route path='/admin/userlist' element={<UserListScreen/>}></Route>
             <Route path='/admin/user/:id/edit' element={<UserEditScreen/>}></Route>
+            <Route path='/admin/productlist' element={<ProductListScreen/>}></Route>
+            <Route path='/admin/product/:id/edit' element={<ProductEditScreen/>}></Route>
           </Routes>
         </Container>
       </main>
